f851b49a427cab20e35025630e99e1fa32885ef9
thedb <thedb11@gmail.com>
2025-07-10 20:36:48 +0200

    More mac stuff.

--- flake.nix
+++ flake.nix
@@ -397,7 +397,7 @@
               pkg-config
             ]
             ++ optionals (pkgs.stdenv.isLinux) [ stdenv.cc ]
-            ++ optionals (pkgs.stdenv.isDarwin) [ clang ]
+            ++ optionals (pkgs.stdenv.isDarwin) [ libiconv-darwin ]
             ++ linkers
             ++ headers
           );
