7760b364db9fb7c30bc76c91b428900b8fbeffbf
thedb <thedb11@gmail.com>
2025-07-10 20:33:40 +0200

    Trying some more stuff for MacOS compatibility :- )!!

--- flake.nix
+++ flake.nix
@@ -397,6 +397,7 @@
               pkg-config
             ]
             ++ optionals (pkgs.stdenv.isLinux) [ stdenv.cc ]
+            ++ optionals (pkgs.stdenv.isDarwin) [ clang ]
             ++ linkers
             ++ headers
           );
