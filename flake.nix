f225c5e371dce33ce239b526640372321a602c4e
thedb <thedb11@gmail.com>
2025-09-10 12:09:01 +0200

    Forgot 's'.

--- flake.nix
+++ flake.nix
@@ -199,7 +199,7 @@
 
           # Set up MacOS SDK if provided through config.
           MACOS_SDK_DIR="${config.macos.sdk}"
-          IOS_SDK_DIR="${config.ios.dk}"
+          IOS_SDK_DIR="${config.ios.sdk}"
 
           # Set up Windows SDK and CRT if pinning is enabled.
           ${optionalString (config.windows.pin) ''
