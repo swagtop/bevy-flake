1d11730a594f969a2873f7b62fe9436ced03dc35
thedb <thedb11@gmail.com>
2025-10-16 16:29:42 +0200

    Hitting same wall again, trying out the windows targets one last time before abandoning.

--- packages.nix
+++ packages.nix
@@ -226,11 +226,8 @@ in
             # '';
           })
         ) [
-          "x86_64-unknown-linux-gnu"
-          "aarch64-unknown-linux-gnu"
-          "x86_64-apple-darwin"
-          "aarch64-apple-darwin"
-          "wasm32-unknown-unknown"
+          "x86_64-pc-windows-msvc"
+          "aarch64-pc-windows-msvc"
         ]);
       };
     });
