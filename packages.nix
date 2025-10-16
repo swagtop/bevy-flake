aa3f703f0284773bf1dd14ea5c319ecd9c55b2f7
thedb <thedb11@gmail.com>
2025-10-16 16:25:39 +0200

    Trying to change around the args, looks as if they are getting mangled.

--- packages.nix
+++ packages.nix
@@ -214,15 +214,8 @@ in
 
             cargoLock.lockFile = "${src}/Cargo.lock";
 
-            # cargoBuildFlags = [
-            #   "--target ${target}"
-            #   "--profile release"
-            #   "--offline"
-            #   "-j 12"
-            # ];
-            # 
             buildPhase = ''
-              "${target-adapter-package}/bin/cargo" build --release --target ${target} --offline -j 12
+              "${target-adapter-package}/bin/cargo" build --profile release -j 12 --target ${target} --offline 
             '';
 
             CARGO = "${target-adapter-package}/bin/cargo";
