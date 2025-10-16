20ce3faac049d0b27e987f92cb30d9b5dd7bbaaa
thedb <thedb11@gmail.com>
2025-10-16 12:50:10 +0200

    More!

--- packages.nix
+++ packages.nix
@@ -67,7 +67,7 @@ in
               else "\${XDG_CACHE_HOME:-$HOME/.cache/}"
             ) + "bevy-flake";
           in if (windows ? sdk) then (''
-            mkdir -p "${cacheDirBase}${windows.sdk}"
+            mkdir -p "${cacheDirBase}${windows.sdk}/xwin"
             ln -s "${windows.sdk}/crt" "${cacheDirBase}${windows.sdk}/xwin/crt"
             ln -s "${windows.sdk}/sdk" "${cacheDirBase}${windows.sdk}/xwin/sdk"
             export XWIN_CACHE_DIR="${cacheDirBase}${windows.sdk}"
