ea9f08e1c005dd3be67318132f6b43cd3d7acdd2
thedb <thedb11@gmail.com>
2025-11-03 14:27:44 +0100

    Adding back the 'or' in the fenix example, for more convenient switching between channels.

--- templates/fenix/flake.nix
+++ templates/fenix/flake.nix
@@ -24,7 +24,7 @@
         channel = "stable"; # For nightly, use "latest".
       in
         fx.combine (
-          [ fx.${channel}.completeToolchain ]
+          [ (fx.${channel}.completeToolchain or fx.channel.toolchain) ]
           ++ map (target: fx.targets.${target}.${channel}.rust-std) targets
         );
     };
