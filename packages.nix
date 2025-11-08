42c0de8b22cdd087a51bd7879b6ec0b31565a7e4
thedb <thedb11@gmail.com>
2025-11-08 21:30:14 +0100

    Likely no benefit from using buildEnv here, it is really slow and its extra functionality is not needed.

--- packages.nix
+++ packages.nix
@@ -45,9 +45,8 @@ in
     stdenv = mkStdenv pkgs;
 
     windowsSdk = 
-      pkgs.buildEnv {
+      pkgs.symlinkJoin {
         name = "merged-windows-sdk";
-        ignoreCollisions = true;
         paths = [
           pkgs.pkgsCross.x86_64-windows.windows.sdk
           pkgs.pkgsCross.aarch64-windows.windows.sdk
