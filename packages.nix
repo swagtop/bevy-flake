1bbdf9b578a4e125538eb3b755bc39cd0f205512
thedb <thedb11@gmail.com>
2025-11-08 11:59:47 +0100

    Trying this hack to see if this gets the attrs needed.

--- packages.nix
+++ packages.nix
@@ -56,7 +56,7 @@ in
           }
         else
           pkgs.windows.sdk.overrideAttrs {
-            src = windows.pinnedSdk;
+            src = (windows.sdk.src // windows.pinnedSdk);
           };
 
     envWrap = {
