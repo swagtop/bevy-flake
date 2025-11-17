ad1d7351dc50d5d9fa7457e72e2bd2f3e40a4510
thedb <thedb11@gmail.com>
2025-11-18 00:03:53 +0100

    Adding glibc for Linux targets with '-L'.

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
               );
               RUSTFLAGS = concatStringsSep " " [
                 "-C linker=ld.lld"
-                "-C link-arg=--sysroot=${nixpkgs.legacyPackages.${system}.glibc}"
+                "-L ${nixpkgs.legacyPackages.${system}.glibc}"
                 (if system == "aarch64-linux" then
                   "-C link-arg=--dynamic-linker=/lib64/ld-linux-aarch64.so.1"
                 else
