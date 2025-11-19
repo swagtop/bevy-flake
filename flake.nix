02a5171c01106728783c0205260dfef8d4165da9
thedb <thedb11@gmail.com>
2025-11-19 11:16:42 +0100

    Trying out a different method of overriding.

--- flake.nix
+++ flake.nix
@@ -33,29 +33,46 @@
             let
               builtConfig = import ./config.nix { inherit system nixpkgs; };
               inherit (builtConfig) pkgs;
-              config = builtConfig.config // overridedConfig;
+
+              cfgApplied = overridedConfig {
+                inherit pkgs;
+                old = builtConfig.config;
+              };
+
+              config = builtConfig.config // cfgApplied;
             in
             import ./packages.nix {
               inherit pkgs nixpkgs config;
             }
           );
+
           devShells = eachSystem (system: {
             default = nixpkgs.legacyPackages.${system}.mkShell {
               name = "bevy-flake";
               packages = [
                 packages.${system}.rust-toolchain
                 packages.${system}.dioxus-cli
-                # packages.${system}.bevy-cli
               ];
             };
           });
+
           formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
         in
         {
           inherit packages devShells formatter eachSystem;
         };
+
     in
-    makeOverridable mkBf {};
+    let
+      base = makeOverridable mkBf (cfg: {});
+    in
+    base // {
+      configure = cfgFn:
+        base.override (prevFn: (args: cfgFn {
+          inherit (args) pkgs;
+          old = prevFn args;
+        }));
+    };
   # mkBf (_: {
   #   templates = {
   #     nixpkgs = warn "This template does not support any cross-compilation." {
