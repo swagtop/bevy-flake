eb7e4c97260db6fb331f7a50548376a898dd7054
thedb <thedb11@gmail.com>
2026-04-29 10:57:13 +0200

    Adding missing 'lib'.

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ let
     url = "https://github.com/NixOS/flake-compat/archive/65f23138d8d09a92e30f1e5c87611b23ef451bf3.tar.gz";
     sha256 = "sha256:1c5f7vfn205bj4bmkgzgyw9504xh5j7gcwi8jf7yh581bwzlwl71";
   }) { src = ./.; };
-  flakeWithSystem = flakeCompatOutput.configure (
+  flakeWithSystem = flakeCompatOutput.lib.configure (
     { default, ... }:
     {
       sytems = [ system ] ++ default.systems;
