bf71c6dd2fbfb2fd2b1654725c0d87d0f9ebc6c8
thedb <thedb11@gmail.com>
2026-04-29 10:46:11 +0200

    Making sure 'currentSystem' is always in the flake config when importing with flake compat.

--- default.nix
+++ default.nix
@@ -7,5 +7,12 @@ let
     url = "https://github.com/NixOS/flake-compat/archive/65f23138d8d09a92e30f1e5c87611b23ef451bf3.tar.gz";
     sha256 = "sha256:1c5f7vfn205bj4bmkgzgyw9504xh5j7gcwi8jf7yh581bwzlwl71";
   }) { src = ./.; };
+  flakeWithSystem = flakeCompatOutput.configure (
+    { default, ... }:
+    {
+      sytems = [ system ] ++ default.systems;
+    }
+  );
+  configuredFlake = flakeWithSystem.defaultNix.lib.configure config;
 in
-(flakeCompatOutput.defaultNix.lib.configure config).packages.${system}
+(configuredFlake).packages.${system}
